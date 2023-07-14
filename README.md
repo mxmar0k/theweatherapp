675011df3f2f9fb5c9076e1b87f52a1be155d881
Hola Bart <marcoferrusquia@gmail.com>
2023-07-13 22:12:16 -0600

    corrected the html because it was driving me crazy and added varipus functions to have history with local storage

---
+++ README.md
@@ -0,0 +1,54 @@
+Cool Weather App!!
+
+You can find the repository here: https://github.com/mxmar0k/theweatherapp
+
+And you can find the page live at: 
+
+
+![Alt text](image.png)
+
+Hello girls and boys and everyone!
+
+Here is my super cool weather app.
+
+Ok it is not so cool, but anyway Im exited. Because, even tho I have the suspicion that I created too much code, it somehow works.
+
+In this app we used tailwind via node (it's my first time, so be patient and dont laught too much).
+
+The html is based on grid, because honestly i just dont get flex and it was driving me crazy, i ended up changing the whole layout. Grid was much more easier and user friendly. I regret discovering til now.
+
+As you can see there are 4 main grid elements that comunicate with the JS
+
+The first is the search part, were we have an input text for the user to look for a city and a search button.
+
+We have a current weather to append the card of the  current climate
+
+next we have a section for the next 5 days, if you're living in mexico city this does not apply, because the weather never makes sense.
+
+Finally we have a search history which is interactive and you can push on the searched cities and can log the info again updated.
+
+Next we go to the js and oh boy...
+
+The js was modern torture
+
+We have various functions to retrieve/save and load the search history user interface (how fancy). We also added event listeners so when you click it loads the information. 
+
+The main function (i think) is the one where we fetch the long and latitude via the api key. So when we type a city it retrieves that information and we can use that in another api url. 
+
+The other part is get the weather details, but it must be filtered so that we get different days, i actually got it wrong the fist time and i was getting 3 hour lapses hahaha kill me.
+
+Then we have to retrive the information for the array 0, which is the current weather and append it to the html section where it belongs.
+
+We use slice to filter that information.
+
+Then we do the same for the next days, it is basically the same function, but now the slice is 1-6, then we append the cards to the corresponding part of the html:
+
+![Alt text](<Captura de pantalla 2023-07-13 a la(s) 19.08.22.png>)
+
+we have an event listenet to get the user input (the city), to get the coordinates and then we use that coordinates as an input to the getWeatherDetails functions, and we add also to the search history.
+
+The js has a more precise explanation. Hope you enjoy it.
+
+
+
+
--- dist/script.js
+++ dist/script.js
@@ -1,165 +1,246 @@
-//just to see if it works
+// just to see if it works
 console.log("hello world");
 
 const searchButton = document.getElementById("search-button");
 const userInput = document.getElementById("user-input");
 
+// this function retrieves the search history from local storage
+function getSearchHistory() {
+  const searchHistory = localStorage.getItem('searchHistory');
+  return searchHistory ? JSON.parse(searchHistory) : [];
+}
 
-//we added this function to get the weather details of the city we logged
+// this function saves the search history to our one and only local storage
+function saveSearchHistory(history) {
+  localStorage.setItem('searchHistory', JSON.stringify(history));
+}
+
+// we also need a function to update the search history and this is the cool one
+//beacuse it adds an event listener to each city loged
+// I also discovered that you can add clases here and it uses tailwind
+//woooooooooow
+function updateSearchHistoryUI(history) {
+  const searchHistoryList = document.getElementById('search-history');
+  searchHistoryList.innerHTML = '';
+
+  history.forEach(city => {
+    const listItem = document.createElement('li');
+    listItem.textContent = city;
+    listItem.classList.add(
+        'border',
+        'bg-white',
+        'list-none',
+        'rounded-sm',
+        'px-3',
+        'py-3',
+        'cursor-pointer',
+        'hover:text-white',
+        'hover:bg-blue-600',
+        'text-gray-800',
+      );
+    listItem.addEventListener('click', function() {
+      getCoordinates(city)
+        .then(coordinates => {
+          console.log('Coordinates:', coordinates);
+          userInput.value = city;
+          getWeatherDetails(coordinates.latitude, coordinates.longitude);
+        });
+    });
+
+    searchHistoryList.appendChild(listItem);
+  });
+}
+
+// i added another function to add searched city to the history on our local storage
+function addToSearchHistory(city) {
+  const history = getSearchHistory();
+  history.push(city);
+  saveSearchHistory(history);
+  updateSearchHistoryUI(history);
+}
+
+// finally we added load the history when the page..loads 
+window.addEventListener('DOMContentLoaded', function() {
+  const history = getSearchHistory();
+  updateSearchHistoryUI(history);
+});
+
+// we added this function to get the weather details of the city we logged
 // it used the latitude and longitude parameters we obtain further in this code
-//  once we have a successful response we parse it as json and then the fun begins
-// i actually got it wrong the first time, because it gave me the same day with a difference of 3 hours on each array
+// once we have a successful response we parse it as json and then the fun begins
+// i actually got it wrong the first time because it gave me the same day with a difference of 3 hours on each array
 // so i had to change it to filter, we use forEach to go through the data with a unix timestamp, if there is a new entry for a different date, it creates a new one.
 // the forecast contains one entry per day, with the date, temp, wind, humidity, and icon
 // it also contains my tears with how much I have cried doing this
-
+// i had to add a code to clear the current weather and prediction cards when the code loads
 function getWeatherDetails(latitude, longitude) {
-    const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947';
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947';
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Unable to fetch weather details.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const filteredForecast = {};
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Unable to fetch weather details.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const filteredForecast = {};
 
-            data.list.forEach(item => {
-                const date = new Date(item.dt * 1000).toLocaleDateString();
+      data.list.forEach(item => {
+        const date = new Date(item.dt * 1000).toLocaleDateString();
 
-                if (!filteredForecast[date]) {
-                    filteredForecast[date] = {
-                        date: new Date(item.dt * 1000),
-                        temperature: item.main.temp,
-                        wind: item.wind.speed,
-                        humidity: item.main.humidity,
-                        icon: item.weather[0].icon
-                    };
-                }
-            });
+        if (!filteredForecast[date]) {
+          filteredForecast[date] = {
+            date: new Date(item.dt * 1000),
+            temperature: item.main.temp,
+            wind: item.wind.speed,
+            humidity: item.main.humidity,
+            icon: item.weather[0].icon
+          };
+        }
+      });
 
-            console.log('Filtered Forecast:', Object.values(filteredForecast));
+      console.log('Filtered Forecast:', Object.values(filteredForecast));
 
-// i added 2 const for current and forecast weathers, why? because i was going crazy
+      // i added 2 const for current and forecast weathers, why? because i was going crazy
 
-            const currentWeather = document.getElementById('current-weather');
-            currentWeather.innerHTML = '';
+      const currentWeather = document.getElementById('current-weather');
+      currentWeather.innerHTML = '';
 
-            const currentWeatherHeading = document.createElement('h3');
-            currentWeatherHeading.textContent = `in ${userInput.value.trim()}`;
-            currentWeather.appendChild(currentWeatherHeading);
+      const currentWeatherHeading = document.createElement('h3');
+      currentWeatherHeading.textContent = `in ${userInput.value.trim()}`;
+      currentWeather.appendChild(currentWeatherHeading);
 
-            Object.values(filteredForecast).slice(0, 1).forEach(day => {
-                const listItem = document.createElement('li');
-                listItem.classList.add('weather-card');
+      Object.values(filteredForecast).slice(0, 1).forEach(day => {
+        const listItem = document.createElement('li');
+        listItem.classList.add(
+            'border',
+            'bg-white',
+            'list-none',
+            'rounded-sm',
+            'px-3',
+            'py-3',
+            'cursor-pointer',
+            'hover:text-white',
+            'hover:bg-blue-600'
+          );
+        listItem.classList.add('weather-card');
 
-                const dateHeading = document.createElement('h3');
-                dateHeading.textContent = day.date.toLocaleDateString();
-                listItem.appendChild(dateHeading);
+        const dateHeading = document.createElement('h3');
+        dateHeading.textContent = day.date.toLocaleDateString();
+        listItem.appendChild(dateHeading);
 
-                const weatherIcon = document.createElement('img');
-                weatherIcon.src = `https://openweathermap.org/img/wn/${day.icon}.png`;
-                weatherIcon.alt = 'weather-icon';
-                listItem.appendChild(weatherIcon);
+        const weatherIcon = document.createElement('img');
+        weatherIcon.src = `https://openweathermap.org/img/wn/${day.icon}.png`;
+        weatherIcon.alt = 'weather-icon';
+        listItem.appendChild(weatherIcon);
 
-                const temperatureHeading = document.createElement('h3');
-                temperatureHeading.textContent = `Temp: ${day.temperature}°F`;
-                listItem.appendChild(temperatureHeading);
+        const temperatureHeading = document.createElement('h3');
+        temperatureHeading.textContent = `Temp: ${day.temperature}°F`;
+        listItem.appendChild(temperatureHeading);
 
-                const windHeading = document.createElement('h3');
-                windHeading.textContent = `Wind: ${day.wind} MPH`;
-                listItem.appendChild(windHeading);
+        const windHeading = document.createElement('h3');
+        windHeading.textContent = `Wind: ${day.wind} MPH`;
+        listItem.appendChild(windHeading);
 
-                const humidityHeading = document.createElement('h3');
-                humidityHeading.textContent = `Humidity: ${day.humidity}%`;
-                listItem.appendChild(humidityHeading);
+        const humidityHeading = document.createElement('h3');
+        humidityHeading.textContent = `Humidity: ${day.humidity}%`;
+        listItem.appendChild(humidityHeading);
 
-                currentWeather.appendChild(listItem);
-            });
+        currentWeather.appendChild(listItem);
+      });
 
-            const predictionCards = document.getElementById('prediction-cards');
-            predictionCards.innerHTML = '';
+      const predictionCards = document.getElementById('prediction-cards');
+      predictionCards.innerHTML = '';
 
-            Object.values(filteredForecast).slice(1, 6).forEach(day => {
-                const listItem = document.createElement('li');
-                listItem.classList.add('weather-card');
+      Object.values(filteredForecast).slice(1, 6).forEach(day => {
+        const listItem = document.createElement('li');
+        listItem.classList.add(
+            'border',
+            'bg-white',
+            'list-none',
+            'rounded-sm',
+            'px-3',
+            'py-3',
+            'cursor-pointer',
+            'hover:text-white',
+            'hover:bg-blue-600',
+          );
+        listItem.classList.add('weather-card');
 
-                const dateHeading = document.createElement('h3');
-                dateHeading.textContent = day.date.toLocaleDateString();
-                listItem.appendChild(dateHeading);
+        const dateHeading = document.createElement('h3');
+        dateHeading.textContent = day.date.toLocaleDateString();
+        listItem.appendChild(dateHeading);
 
-                const weatherIcon = document.createElement('img');
-                weatherIcon.src = `https://openweathermap.org/img/wn/${day.icon}.png`;
-                weatherIcon.alt = 'weather-icon';
-                listItem.appendChild(weatherIcon);
+        const weatherIcon = document.createElement('img');
+        weatherIcon.src = `https://openweathermap.org/img/wn/${day.icon}.png`;
+        weatherIcon.alt = 'weather-icon';
+        listItem.appendChild(weatherIcon);
 
-                const temperatureHeading = document.createElement('h3');
-                temperatureHeading.textContent = `Temp: ${day.temperature}°F`;
-                listItem.appendChild(temperatureHeading);
+        const temperatureHeading = document.createElement('h3');
+        temperatureHeading.textContent = `Temp: ${day.temperature}°F`;
+        listItem.appendChild(temperatureHeading);
 
-                const windHeading = document.createElement('h3');
-                windHeading.textContent = `Wind: ${day.wind} MPH`;
-                listItem.appendChild(windHeading);
+        const windHeading = document.createElement('h3');
+        windHeading.textContent = `Wind: ${day.wind} MPH`;
+        listItem.appendChild(windHeading);
 
-                const humidityHeading = document.createElement('h3');
-                humidityHeading.textContent = `Humidity: ${day.humidity}%`;
-                listItem.appendChild(humidityHeading);
+        const humidityHeading = document.createElement('h3');
+        humidityHeading.textContent = `Humidity: ${day.humidity}%`;
+        listItem.appendChild(humidityHeading);
 
-                predictionCards.appendChild(listItem);
-            });
+        predictionCards.appendChild(listItem);
+      });
 
-            return {
-                forecast: Object.values(filteredForecast)
-            };
-        })
-        .catch(error => {
-            console.log('Error:', error);
-        });
+      return {
+        forecast: Object.values(filteredForecast)
+      };
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-//we have a function to get coordinates for a city
+// we have a function to get coordinates for a city
 // the url has parameters to limit to one the response
 // there is a const with the api so it logs with the url
-//the function sends a fetch and receives the json data to extract the lat
+// the function sends a fetch and receives the json data to extract the lat
 // and lon, if they are not found it throws an error
 function getCoordinates(city) {
-    const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947';
-    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947';
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // just to check what it responses
-            console.log('API Response:', data);
-            if (data.length > 0) {
-                const { lat, lon } = data[0];
-                return { latitude: lat, longitude: lon };
-            } else {
-                throw new Error('Coordinates not found for the given city.');
-            }
-        })
-        .catch(error => {
-            console.log('Error:', error);
-        });
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // just to check what it responses
+      console.log('API Response:', data);
+      if (data.length > 0) {
+        const { lat, lon } = data[0];
+        return { latitude: lat, longitude: lon };
+      } else {
+        throw new Error('Coordinates not found for the given city.');
+      }
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-//this is an event listener added to the search button, it triggers
-// the coordinates function and trims the value of the user input to use it in city
+// Event listener for the search button
 searchButton.addEventListener('click', function() {
-    const city = userInput.value.trim();
+  const city = userInput.value.trim();
 
-    if (city === '') {
-        console.log('Please enter a valid city name.');
-        return;
-    }
+  if (city === '') {
+    console.log('Please enter a valid city name.');
+    return;
+  }
 
-    getCoordinates(city)
-        .then(coordinates => {
-            console.log('Coordinates:', coordinates);
-            getWeatherDetails(coordinates.latitude, coordinates.longitude);
-        });
-
+  getCoordinates(city)
+    .then(coordinates => {
+      console.log('Coordinates:', coordinates);
+      getWeatherDetails(coordinates.latitude, coordinates.longitude);
+      addToSearchHistory(city);
+    });
 });
