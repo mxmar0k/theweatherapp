e0ac3bb1343a2c29639487be1e0bb0e316fe09b6
Hola Bart <marcoferrusquia@gmail.com>
2023-07-14 09:30:34 -0600

    added new features because a friend discovered a minor error

--- dist/script.js
+++ dist/script.js
@@ -16,9 +16,9 @@ function saveSearchHistory(history) {
 }
 
 // we also need a function to update the search history and this is the cool one
-//beacuse it adds an event listener to each city loged
-// I also discovered that you can add clases here and it uses tailwind
-//woooooooooow
+// beacuse it adds an event listener to each city logged
+// I also discovered that you can add classes here and it uses tailwind
+// woooooooooow
 function updateSearchHistoryUI(history) {
   const searchHistoryList = document.getElementById('search-history');
   searchHistoryList.innerHTML = '';
@@ -27,18 +27,18 @@ function updateSearchHistoryUI(history) {
     const listItem = document.createElement('li');
     listItem.textContent = city;
     listItem.classList.add(
-        'border',
-        'bg-white',
-        'list-none',
-        'rounded-sm',
-        'px-3',
-        'py-3',
-        'cursor-pointer',
-        'hover:text-white',
-        'hover:bg-blue-600',
-        'text-gray-800',
-      );
-    listItem.addEventListener('click', function() {
+      'border',
+      'bg-white',
+      'list-none',
+      'rounded-sm',
+      'px-3',
+      'py-3',
+      'cursor-pointer',
+      'hover:text-white',
+      'hover:bg-blue-600',
+      'text-gray-800',
+    );
+    listItem.addEventListener('click', function () {
       getCoordinates(city)
         .then(coordinates => {
           console.log('Coordinates:', coordinates);
@@ -59,14 +59,14 @@ function addToSearchHistory(city) {
   updateSearchHistoryUI(history);
 }
 
-// finally we added load the history when the page..loads 
-window.addEventListener('DOMContentLoaded', function() {
+// finally we added load the history when the page..loads
+window.addEventListener('DOMContentLoaded', function () {
   const history = getSearchHistory();
   updateSearchHistoryUI(history);
 });
 
 // we added this function to get the weather details of the city we logged
-// it used the latitude and longitude parameters we obtain further in this code
+// it uses the latitude and longitude parameters we obtain further in this code
 // once we have a successful response we parse it as json and then the fun begins
 // i actually got it wrong the first time because it gave me the same day with a difference of 3 hours on each array
 // so i had to change it to filter, we use forEach to go through the data with a unix timestamp, if there is a new entry for a different date, it creates a new one.
@@ -108,23 +108,39 @@ function getWeatherDetails(latitude, longitude) {
       const currentWeather = document.getElementById('current-weather');
       currentWeather.innerHTML = '';
 
+      //added functionality to the reverse geocoding and tailwind styles to make it look better
+      //i actually dont know why it appends to the bottom, but i had to rode with it haha
+
       const currentWeatherHeading = document.createElement('h3');
-      currentWeatherHeading.textContent = `in ${userInput.value.trim()}`;
-      currentWeather.appendChild(currentWeatherHeading);
+      getCoordinates(userInput.value.trim())
+        .then(coordinates => {
+          getCityName(coordinates.latitude, coordinates.longitude)
+            .then(cityName => {
+              currentWeatherHeading.textContent = `in ${cityName}`;
+              currentWeatherHeading.classList.add(
+                'text-2xl',
+                'font-bold',
+                'text-gray-800',
+                'mb-4',
+                'text-center',
+              );
+              currentWeather.appendChild(currentWeatherHeading);
+            });
+        });
 
       Object.values(filteredForecast).slice(0, 1).forEach(day => {
         const listItem = document.createElement('li');
         listItem.classList.add(
-            'border',
-            'bg-white',
-            'list-none',
-            'rounded-sm',
-            'px-3',
-            'py-3',
-            'cursor-pointer',
-            'hover:text-white',
-            'hover:bg-blue-600'
-          );
+          'border',
+          'bg-white',
+          'list-none',
+          'rounded-sm',
+          'px-3',
+          'py-3',
+          'cursor-pointer',
+          'hover:text-white',
+          'hover:bg-blue-600'
+        );
         listItem.classList.add('weather-card');
 
         const dateHeading = document.createElement('h3');
@@ -157,16 +173,16 @@ function getWeatherDetails(latitude, longitude) {
       Object.values(filteredForecast).slice(1, 6).forEach(day => {
         const listItem = document.createElement('li');
         listItem.classList.add(
-            'border',
-            'bg-white',
-            'list-none',
-            'rounded-sm',
-            'px-3',
-            'py-3',
-            'cursor-pointer',
-            'hover:text-white',
-            'hover:bg-blue-600',
-          );
+          'border',
+          'bg-white',
+          'list-none',
+          'rounded-sm',
+          'px-3',
+          'py-3',
+          'cursor-pointer',
+          'hover:text-white',
+          'hover:bg-blue-600',
+        );
         listItem.classList.add('weather-card');
 
         const dateHeading = document.createElement('h3');
@@ -208,6 +224,9 @@ function getWeatherDetails(latitude, longitude) {
 // the function sends a fetch and receives the json data to extract the lat
 // and lon, if they are not found it throws an error
 function getCoordinates(city) {
+  if (typeof city !== "string") {
+    throw new Error('City must be a string');
+  }
   const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947';
   const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
@@ -228,8 +247,31 @@ function getCoordinates(city) {
     });
 }
 
+// function to get the city name from coordinates using reverse geocoding
+// i added this, because a friend told me when she typed 1, it actually gave coordinates, turns out 
+//it is a district in viena, so i think it was better to use reverse geo, to give me the location of the city
+//rather than a district, it was basically the same code we have been using all over
+function getCityName(latitude, longitude) {
+  const apiKey = '9a8b08fd63683cdb4192ebb6f7b26947'; 
+  const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length > 0) {
+        const { name } = data[0];
+        return name;
+      } else {
+        throw new Error('City name not found for the given coordinates.');
+      }
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
+
 // Event listener for the search button
-searchButton.addEventListener('click', function() {
+searchButton.addEventListener('click', function () {
   const city = userInput.value.trim();
 
   if (city === '') {
